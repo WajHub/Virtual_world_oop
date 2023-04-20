0f4d26c5feba7251b1ed37da65573f5a9268d295
WajdaHub <hubert.wajda03@gmail.com>
2023-04-20 02:57:57 +0200

    creating menu to select objects

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -1,17 +1,6 @@
 #include <conio.h>
 #include "mygotoxy.h"
 #include "World.h"
-#include "Body/Animals/Wolf.h"
-#include "Body/Animals/Sheep.h"
-#include "Body/Animals/Fox.h"
-#include "Body/Animals/Turtle.h"
-#include "Body/Animals/Antelope.h"
-#include "Body/Plants/Grass.h"
-#include "Body/Plants/Guarana.h"
-#include "Body/Plants/Pine_hogweed.h"
-#include "Body/Plants/Sonchus.h"
-#include "Body/Plants/Wolfberries.h"
-#include "Body/Animals/Human.h"
 
 int main() {
     ShowConsoleCursor(false);
